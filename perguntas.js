bc2ec0a70482c5fc5eaaec49e3507b6baa6340cc
Lucas <127355311+LeherLucas@users.noreply.github.com>
2024-09-25 09:40:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Programação',
+    'O que é uma classe?',
+    'É um tipo de seletor HTML/CSS.'
+)
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'É uma linguagem de programação utilizada para tornar uma página dinâmica.'
+)
+
+criaCartao(
+    'Química',
+    'Qual o elemento que possuí apenas 1 elétron?',
+    'Hidrogênio'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'É um bloco de códigos que executa uma ação.'
+)
+
+criaCartao(
+    'Língua Chinesa',
+    'Qual a diferença entre mandarim e chinês',
+    'Mandarim é a língua falada, ja chinês é a escrita'
+)
